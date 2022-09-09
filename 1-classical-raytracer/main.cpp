adcc8ebcbbae34a2758934f5512b9fec9ceb5427
yumcyawiz <hashdolf1923@gmail.com>
2022-09-09 19:32:21 +0900

    remove vec3, use glm

--- 1-classical-raytracer/main.cpp
+++ 1-classical-raytracer/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 
-#include "vec3.h"
+#include "glm/glm.hpp"
 
 int main()
 {
-  Vec3 v1(0, 0, 0);
-  Vec3 v2(1, 1, 1);
-  const Vec3 v = v1 + v2;
+  glm::vec3 v1(0, 0, 0);
+  glm::vec3 v2(1, 1, 1);
+  const glm::vec3 v = v1 + v2;
   printf("%f %f %f\n", v.x, v.y, v.z);
   return 0;
 }
