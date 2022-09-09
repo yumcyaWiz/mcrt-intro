5f2db970278480da75b219da362c5245dd1180f3
yumcyawiz <hashdolf1923@gmail.com>
2022-09-09 19:15:25 +0900

    add vec3 operators

--- 1-classical-raytracer/main.cpp
+++ 1-classical-raytracer/main.cpp
@@ -1,3 +1,12 @@
+#include <cstdio>
+
 #include "vec3.h"
 
-int main() { return 0; }
+int main()
+{
+  Vec3 v1(0, 0, 0);
+  Vec3 v2(1, 1, 1);
+  const Vec3 v = v1 + v2;
+  printf("%f %f %f\n", v.x, v.y, v.z);
+  return 0;
+}
