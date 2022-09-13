cc898112a5eda565a5c7dcb0a93d9d859e44ec92
yumcyawiz <hashdolf1923@gmail.com>
2022-09-14 04:48:05 +0900

    impl build orthonormal basis

--- 2-simple-pt/reference/include/core.h
+++ 2-simple-pt/reference/include/core.h
@@ -32,17 +32,33 @@ struct IntersectInfo {
 struct Material {
   glm::vec3 kd;     // diffuse color
   glm::vec3 ks;     // specular color
+  glm::vec3 ke;     // emission color
   float roughness;  // specular roughness
 
-  Material(const glm::vec3& kd_, const glm::vec3& ks_, float roughness_)
+  Material(const glm::vec3& kd_, const glm::vec3& ks_, const glm::vec3& ke,
+           float roughness_)
       : kd(kd_), ks(ks_), roughness(roughness_)
   {
   }
 };
 
+// convert spherical coordinate to cartesian coordinate
 inline glm::vec3 spherical_to_cartesian(float phi, float theta)
 {
   const float sin_theta = glm::sin(theta);
   return glm::vec3(glm::cos(phi) * sin_theta, glm::cos(theta),
                    glm::sin(phi) * sin_theta);
+}
+
+// Duff, T., Burgess, J., Christensen, P., Hery, C., Kensler, A., Liani, M., &
+// Villemin, R. (2017). Building an orthonormal basis, revisited. JCGT, 6(1).
+inline void orthonormal_basis(const glm::vec3& normal, glm::vec3& tangent,
+                              glm::vec3& bitangent)
+{
+  float sign = std::copysignf(1.0f, normal.z);
+  const float a = -1.0f / (sign + normal.z);
+  const float b = normal.x * normal.y * a;
+  tangent = glm::vec3(1.0f + sign * normal.x * normal.x * a, sign * b,
+                        -sign * normal.x);
+  bitangent = glm::vec3(b, sign + normal.y * normal.y * a, -normal.y);
 }
