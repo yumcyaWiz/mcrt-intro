34096441c20e33a12e7a9c2bd78e974ff436eb74
yumcyawiz <hashdolf1923@gmail.com>
2022-09-26 21:51:04 +0900

    impl load hdr texture

--- 3-pt/reference/include/texture.h
+++ 3-pt/reference/include/texture.h
@@ -12,6 +12,30 @@ class Texture
  public:
   Texture(const std::filesystem::path& filepath)
   {
+    const std::string extension = filepath.extension();
+    if (extension == ".jpg" || extension == ".png") {
+      loadLDR(filepath);
+    } else if (extension == ".hdr") {
+    } else {
+      throw std::runtime_error("unsupported extension");
+    }
+  }
+
+  glm::vec4 fetch(const glm::vec2& texcoord) const
+  {
+    const int i = (m_width - 1) * glm::clamp(texcoord.x, 0.0f, 1.0f);
+    const int j = (m_height - 1) * glm::clamp(texcoord.y, 0.0f, 1.0f);
+    return m_data[i + m_width * j];
+  }
+
+ private:
+  int m_width;                    // width of texture
+  int m_height;                   // height of texture
+  std::vector<glm::vec4> m_data;  // texture image data(RGBA)
+
+  // load jpeg, png image
+  void loadLDR(const std::filesystem::path& filepath)
+  {
     spdlog::info("[Texture] loading {}", filepath.generic_string());
 
     stbi_set_flip_vertically_on_load(true);
@@ -39,15 +63,31 @@ class Texture
     stbi_image_free(img);
   }
 
-  glm::vec4 fetch(const glm::vec2& texcoord) const
+  // load hdr image
+  void loadHDR(const std::filesystem::path& filepath)
   {
-    const int i = (m_width - 1) * glm::clamp(texcoord.x, 0.0f, 1.0f);
-    const int j = (m_height - 1) * glm::clamp(texcoord.y, 0.0f, 1.0f);
-    return m_data[i + m_width * j];
-  }
+    spdlog::info("[Texture] loading {}", filepath.generic_string());
 
- private:
-  int m_width;                    // width of texture
-  int m_height;                   // height of texture
-  std::vector<glm::vec4> m_data;  // texture image data(RGBA)
+    stbi_set_flip_vertically_on_load(true);
+
+    // load image with stb image
+    int c;
+    float* img =
+        stbi_loadf(filepath.c_str(), &m_width, &m_height, &c, STBI_rgb_alpha);
+    if (!img) {
+      spdlog::error("{}", stbi_failure_reason());
+      throw std::runtime_error("failed to load " + filepath.generic_string());
+    }
+
+    m_data.resize(m_width * m_height);
+    for (int j = 0; j < m_height; ++j) {
+      for (int i = 0; i < m_width; ++i) {
+        const int idx = 4 * i + 4 * m_width * j;
+        m_data[i + m_width * j] =
+            glm::vec4(img[idx + 0], img[idx + 1], img[idx + 2], img[idx + 3]);
+      }
+    }
+
+    stbi_image_free(img);
+  }
 };
